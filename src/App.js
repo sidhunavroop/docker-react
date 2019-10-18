5afc64ada09491856d1d714e0bc6e3df31387e09
Navroop Singh Sidhu <sidhunavroop@yahoo.com>
2019-10-17 20:35:03 -0400

    making changes on feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I am trying to learn react
+          Making changes on feature branch
         </p>
         <a
           className="App-link"
